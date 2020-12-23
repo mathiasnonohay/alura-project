dcf830e59b98d88a6758a3070182cf7289585d70
Mathias Nonohay <mathias12300@gmail.com>
2020-12-23 16:09:37 -0300

    gesture action adicionado à tela principal

--- alura-project/Controllers/RefeicoesTableViewController.swift
+++ alura-project/Controllers/RefeicoesTableViewController.swift
@@ -10,9 +10,13 @@ import UIKit
 
 class RefeicoesTableViewController: UITableViewController, AdicionaRefeicaoDelegate {
     
+    // MARK: - Atributos
+    
     var refeicoes = [Refeicao(nome: "Macarrão", felicidade: 4),
                      Refeicao(nome: "Pizza", felicidade: 3),
                      Refeicao(nome: "Comida Japonesa", felicidade: 5)]
+    
+    // MARK: - Métodos
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return refeicoes.count
@@ -23,12 +27,28 @@ class RefeicoesTableViewController: UITableViewController, AdicionaRefeicaoDeleg
         let refeicao = refeicoes[indexPath.row]
         celula.textLabel?.text = refeicao.nome
         
+        let longPress = UILongPressGestureRecognizer(target: self, action: #selector(mostrarDetalhes(_:)))
+        celula.addGestureRecognizer(longPress)
+        
         return celula
     }
     
     func add(_ refeicao: Refeicao){
         refeicoes.append(refeicao)
         tableView.reloadData()
+    }
+    
+    @objc func mostrarDetalhes(_ gestture: UILongPressGestureRecognizer) {
+        if gestture.state == .began {
+            let celula = gestture.view as! UITableViewCell
+            guard let indexPath = tableView.indexPath(for: celula) else { return }
+            let refeicao = refeicoes[indexPath.row]
+            
+            let alerta = UIAlertController(title: refeicao.nome, message: refeicao.detalhes(), preferredStyle: .alert)
+            let botaoCancelar = UIAlertAction(title: "Ok", style: .cancel, handler: nil)
+            alerta.addAction(botaoCancelar)
+            present(alerta, animated: true, completion: nil)
+        }
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
--- alura-project/Models/Refeicao.swift
+++ alura-project/Models/Refeicao.swift
@@ -32,4 +32,13 @@ class Refeicao: NSObject {
         }
         return total
     }
+    
+    func detalhes() -> String {
+        var mensagem = "Felicidade: \(felicidade)"
+        
+        for item in itens {
+            mensagem += ", \(item.nome) - Calorias: \(item.calorias)"
+        }
+        return mensagem
+    }
 }
